27f6898a04aa4a4ca589fac3e2efcda575208e6e
ericcarvlh <ericcarvalho935@gmail.com>
2022-11-07 23:27:14 -0300

    codificação da logica para consulta deṕossivel email em uso

--- get/verificaSeEmailEstaEmUso.php
+++ get/verificaSeEmailEstaEmUso.php
@@ -1,8 +1,13 @@
 <?php
 
+$host = "sql10.freemysqlhosting.net";
+$username = "sql10550742";
+$password = "iAVbBk1fSD";
+$dbname = "sql10550742";
+
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     
-    $connection_string = mysqli_connect("db4free.net", "monkersenter", "47735428", "db_monkers_enter")
+    $connection_string = mysqli_connect($host, $username, $password, $dbname)
     or die 
     ("Problema ao conecter-se ao servidor.");
 
@@ -10,9 +15,17 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
     if (!$connection_string) {
         echo "Não foi possível conectar ao banco MySQL."; 
+        echo '<br>';
         exit;
     } else {
-        echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.";
+        echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!";
+        echo '<br>';
+
+        $cmd = "select * from  tbl_usuario where email_usuario = '$email_usuario'";
+        $result = mysqli_query($connection_string, $cmd); 
+        $rows = mysqli_num_rows($result);
+        echo $rows;
+
     }
 
     mysqli_close($connection_string);
