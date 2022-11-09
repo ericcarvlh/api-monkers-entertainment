805f86f617d3dfa456f66bc06c078b2a398984ef
ericcarvlh <ericcarvalho935@gmail.com>
2022-11-09 18:21:32 -0300

    codificação da api de login

--- get/realizaLogin.php
+++ get/realizaLogin.php
@@ -1,8 +1,13 @@
 <?php
 
+$host = "sql10.freemysqlhosting.net";
+$username = "sql10550742";
+$password = "iAVbBk1fSD";
+$dbname = "sql10550742";
+
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     
-    $connection_string = mysqli_connect("db4free.net", "monkersenter", "47735428", "db_monkers_enter")
+    $connection_string = mysqli_connect($host, $username, $password, $dbname)
     or die 
     ("Problema ao conecter-se ao servidor.");
 
@@ -14,7 +19,22 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         echo "Não foi possível conectar ao banco MySQL."; 
         exit;
     } else {
-        echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.";
+        $cmd = "Select * from tbl_Usuario 
+        where email_usuario = '$email_usuario' and senha_usuario = '$senha_usuario'";
+
+        $result = mysqli_query($connection_string, $cmd); 
+        $busca_por_email_e_senha = mysqli_num_rows($result);
+
+        $cmd = "Select * from tbl_Usuario 
+        where nome_usuario = '$nm_usuario' and senha_usuario = '$senha_usuario'";
+
+        $result = mysqli_query($connection_string, $cmd); 
+        $busca_por_nome_usuario_e_senha = mysqli_num_rows($result);
+
+        if($busca_por_email_e_senha != 0 || $busca_por_nome_usuario_e_senha != 0) 
+            echo "True";
+        else
+            echo "False";
     }
 
     mysqli_close($connection_string);
